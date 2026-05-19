bdddac318da580f393fc2be9d683faaf9aa75ee1
Sakib Ahmed <sakibahmedsadhin@gmail.com>
2026-05-19 18:40:51 +0000

    feat: mount appointment routes in server

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/doctors', require('./src/routes/doctors'));
+app.use('/api/appointments', require('./src/routes/appointments'));
 app.use('/api/auth', require('./src/routes/auth'));
 
 // Health check
